11f7ed022be174bebb64b52193233a926b823ded
Handi Heraldo <handiheraldo@gmail.com>
2026-06-26 20:59:49 +0700

    Switch to GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gereja-advent-test/',
   plugins: [
     react(),
     tailwindcss(),
